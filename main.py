25be69a62c332d4a1bdd18f64429bc77089e62d3
Ian Paterson <ianhpaterson@gmail.com>
2022-12-15 16:33:38 -0500

    Add main

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+keys = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+chords = ['I', 'IV', 'V']
+minor_chords = ['ii', 'iii', 'vi']
+
+key = random.choice(keys)
+select_chords = ['I']
+
+for x in range(2):
+    select_chords.append(random.choice(chords + minor_chords))
+
+select_chords.append(random.choice(chords))
+
+print('Key: ', key)
+
+print('Chords:', select_chords)
+
+print('# Strums: ', random.randint(4, 8))
